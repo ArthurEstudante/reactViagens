89c51d454e216d65ceb4a37fedf346359ddbcc95
ARTHUR D'EÇA MORGENSTERN VINHAS MARCHETTI <arthur_d_marchetti@estudante.sesisenai.org.br>
2024-08-06 11:48:02 -0300

    Adiciona 10 destinos pré-cadastrados

--- src/destino.jsx
+++ src/destino.jsx
@@ -1,15 +1,11 @@
-// Destinos.jsx
-import React from 'react';
-import Paises from "./components/paises"
-
-const Destinos = () => {
+// Destino.jsx
+const Destino = ({ nome, descricao }) => {
     return (
-        <>
-            <div>
-                <h2>Destinos</h2><p>Veja nossos destinos incríveis.</p>
-            </div>
-            <Paises />
-        </>)
-};
-
-export default Destinos;
+      <div className="destino">
+        <h2>{nome}</h2>
+        <p>{descricao}</p>
+      </div>
+    );
+  };
+  
+  export default Destino;
--- src/formDestino.jsx
+++ src/formDestino.jsx
@@ -1,4 +1,3 @@
-// FormDestino.jsx
 import React, { useState } from 'react';
 
 const FormDestino = ({ adicionarDestino }) => {
